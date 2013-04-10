e52e68d37535a772f17edb4d1836654c81795fea
Matt Lantz <mattlantz@gmail.com>
2013-04-10 10:32:22 -0400

    General cleaning and recoordinating the directories

--- application/views/core/accounts/unc.php
+++
@@ -1,8 +0,0 @@
-<?php   
-        // Confused yet? No worries, this is just a quick reference page for the user name checker. 
-        // Its what allows the checkmark beside the username when making a user. 
-        // Written by: Matt Lantz
-?>
-<?php 
-    echo $result;
-?>
