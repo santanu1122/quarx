6b7791ad33c1ead21887d78fcae73f8282a46af6
mlantz <mattlantz@gmail.com>
2014-03-27 10:36:19 -0400

    More cleanup

--- js/views/users/add-user.js
+++ js/views/users/add-user.js
@@ -16,19 +16,19 @@ function validationCheck(name, userNameURL){
         success: function(data){
             if (data == 1) {
                 $('#u_name').parent().css({
-                    border: '1px solid #f00'
+                    boxShadow: 'inset 0px 1px 4px #f00'
                 });
                 $('#addBtn').attr('disabled', 'disabled');
             } else {
                 $('#u_name').parent().css({
-                    border: '1px solid #61B329'
+                    boxShadow: 'inset 0px 1px 4px #61B329'
                 });
                 $('#addBtn').removeAttr('disabled');
             }
 
             if ($('#u_name').val().length < 4) {
                 $('#u_name').parent().css({
-                    border: '1px solid #f00'
+                    border: 'inset 0px 1px 4px #f00 !important'
                 });
                 $('#addBtn').attr('disabled', 'disabled');
             }
