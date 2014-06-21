81fcfa4342dfe07dec9f9ca223a37d89bdc77f2e
mlantz <mattlantz@gmail.com>
2014-06-20 23:23:51 -0400

    Updated sample model

---
+++ application/modules/sample/models/model_sample.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Quarx
+ *
+ * A modular application framework built on CodeIgniter
+ *
+ * @package     Quarx
+ * @author      Matt Lantz
+ * @copyright   Copyright (c) 2013 Matt Lantz
+ * @license     http://ottacon.co/quarx/license.html
+ * @link        http://ottacon.co/quarx
+ * @since       Version 1.0
+ * 
+ */
+
+class model_sample extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    
+}
+
+// End of File
+/* Location: /models/ */
+?>
